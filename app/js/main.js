b8ace9f63277ee1d605e8cc5372c92998bb1292e
Thomas Heaton <thomasaheaton@gmail.com>
2015-10-20 21:50:50 -0400

    initial commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,36 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// Constructors
-// Only add code to *THIS* section!
+//Constructors
 
-// ????????
-// ????????
-// ????????
-
-// Do not ADD or MODIFY code below this line :)
-// Dogs
-
+//Dog Constructor
 'use strict';
 
+var Dog = function Dog(parameters) {
+  parameters = parameters || {};
+
+  //this.color, hungry, status, owner, cool
+  this.color = parameters.color;
+  this.hungry = parameters.hungry !== undefined ? parameters.hungry : true;
+  this.status = 'normal';
+  this.owner = parameters.owner;
+  this.cool = parameters.cool;
+};
+//Human Constructor
+var Human = function Human(these) {
+  these = these || {};
+
+  //this.cool, feed, pet
+  this.cool = these.cool !== undefined ? these.cool : false;
+
+  this.feed = function (Dog) {
+    Dog.hungry = false;
+  };
+
+  this.pet = function (Dog) {
+    Dog.status = 'happy';
+  };
+};
+// Do not ADD or MODIFY code below this line :)
+// Dogs
 var sadie = new Dog({
   color: 'black',
   hungry: false
@@ -23,11 +43,62 @@ var moonshine = new Dog({
 var atticus = new Dog();
 
 // Humans
-
 var mason = new Human();
 
 var julia = new Human({
   cool: true
+});
+
+var it = function it(description, contents) {
+  console.log('\n\n"It ' + description + '"');
+  contents();
+};
+
+var expect = function expect(target) {
+  return {
+    toBe: function toBe(expectation) {
+      if (target === expectation) {
+        console.log('\n     %cPASSED', 'color:green;', 'Expected', target, 'to be', expectation);
+        return true;
+      } else {
+        console.log('\n     %cFAILED', 'color:red;', 'Expected', target, 'to be', expectation);
+        return false;
+      }
+    }
+  };
+};
+
+// Tests
+it("should make Sadie happy when Mason pets her", function () {
+  expect(sadie.status).toBe('normal');
+  mason.pet(sadie);
+  expect(sadie.status).toBe('happy');
+});
+
+it("should make Sadie black", function () {
+  expect(sadie.color).toBe('black');
+});
+
+it("should make Moonshine hungry and Sadie not hungry", function () {
+  expect(moonshine.hungry).toBe(true);
+  expect(sadie.hungry).toBe(false);
+});
+
+it("should make Moonshine no longer hungry when you feed him", function () {
+  julia.feed(moonshine);
+  expect(moonshine.hungry).toBe(false);
+});
+
+it("should not affect Atticus and Moonshine's owner properties when setting Mason as Sadie's owner ", function () {
+  sadie.owner = mason;
+  expect(moonshine.owner).toBe(undefined);
+  expect(atticus.owner).toBe(undefined);
+});
+
+it("should make Julia cool and Mason not cool", function () {
+  sadie.owner = mason;
+  expect(julia.cool).toBe(true);
+  expect(mason.cool).toBe(false);
 });
 
 },{}]},{},[1])
--- js/main.js
+++ js/main.js
@@ -1,29 +1,101 @@
-// Constructors
-// Only add code to *THIS* section!
+//Constructors
 
-// ????????
-// ????????
-// ????????
+//Dog Constructor
+let Dog = function(parameters) {
+  parameters = parameters || {};
 
+  //this.color, hungry, status, owner, cool
+  this.color = parameters.color; 
+  this.hungry = (parameters.hungry !== undefined) ? parameters.hungry : true;
+  this.status = 'normal';
+  this.owner = parameters.owner; 
+  this.cool = parameters.cool;  
+};
+//Human Constructor
+let Human = function (these) {
+  these = these || {};
+
+  //this.cool, feed, pet
+  this.cool = (these.cool !== undefined) ? these.cool : false;
+
+  this.feed = function (Dog) {
+    Dog.hungry = false;
+  };
+
+  this.pet = function(Dog) {
+    Dog.status = 'happy';
+  };
+};
 // Do not ADD or MODIFY code below this line :)
 // Dogs
-
-var sadie = new Dog({
+let sadie = new Dog({
   color: 'black',
   hungry: false,
 });
 
-var moonshine = new Dog({
+let moonshine = new Dog({
   color: 'blue-red',
 });
 
-var atticus = new Dog();
-
+let atticus = new Dog();
 
 // Humans
+let mason = new Human();
 
-var mason = new Human();
-
-var julia = new Human({
+let julia = new Human({
   cool: true,
+});
+
+
+let it = function (description, contents) {
+  console.log('\n\n"It ' + description + '"');
+  contents();
+};
+
+let expect = function (target) {
+  return {
+    toBe: function(expectation) {
+      if (target === expectation) {
+        console.log('\n     %cPASSED', 'color:green;', 'Expected', target, 'to be', expectation);
+        return true;
+      } else {
+        console.log('\n     %cFAILED', 'color:red;', 'Expected', target, 'to be', expectation);
+        return false;
+      }
+    }
+  };
+};
+
+// Tests
+it("should make Sadie happy when Mason pets her", function() {
+  expect(sadie.status).toBe('normal');
+  mason.pet(sadie);
+  expect(sadie.status).toBe('happy');
+});
+
+it("should make Sadie black", function(){
+  expect(sadie.color).toBe('black');
+});
+
+it("should make Moonshine hungry and Sadie not hungry", function() {
+  expect(moonshine.hungry).toBe(true);
+  expect(sadie.hungry).toBe(false);
+});
+
+it("should make Moonshine no longer hungry when you feed him", function() {
+  julia.feed(moonshine);
+  expect(moonshine.hungry).toBe(false);
+});
+
+
+it("should not affect Atticus and Moonshine's owner properties when setting Mason as Sadie's owner ", function() {
+  sadie.owner = mason;
+  expect(moonshine.owner).toBe(undefined);
+  expect(atticus.owner).toBe(undefined);
+});
+
+it("should make Julia cool and Mason not cool", function() {
+  sadie.owner = mason;
+  expect(julia.cool).toBe(true);
+  expect(mason.cool).toBe(false);
 });
